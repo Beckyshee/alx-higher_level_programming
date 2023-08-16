a64a1f87b9720f0adc5ccd74c649c9b79468a60f
Rebecca Kariuki <97875510+Beckyshee@users.noreply.github.com>
2023-08-16 22:02:10 +0300

    a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,5 +1,6 @@
 -- Lists all genres by their rating
 -- Records are ordered by descending rating
+
 SELECT `name`, SUM(`rate`) AS `rating`
   FROM `tv_genres` AS g
        INNER JOIN `tv_show_genres` AS s
