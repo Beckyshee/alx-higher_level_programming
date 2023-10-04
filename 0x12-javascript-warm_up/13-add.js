b49151e5fa6bc4c856ec8553a6fd96158f2c51d2
Rebecca Kariuki <97875510+Beckyshee@users.noreply.github.com>
2023-10-04 08:42:33 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/13-add.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-//exports.add = (a, b) => a + b;
-exports.add = function(a, b){
-return a + b;
-};
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const args = require('process').argv;
-const myVar = parseInt(args[2]);
-if (!isNaN(myVar)) {
-  console.log('My number: ' + myVar);
-} else {
-  console.log('Not a number');
-}
